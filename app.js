f516dd176dc8df850ff64ddaa5ef22103298a470
MadsOverggard <nissum_10@hotmail.com>
2023-11-15 08:55:30 +0100

    Init project
    Added package and app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+// Imports
+import express, { response } from 'express';
+
+// Consts
+const app = express();
+const port = 80;
+
+// Middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Routes
+app.get('/', (request, response) => {
+    response.send('Hello World!');
+})
+
+
+// Listen for connection
+app.listen(port, () => console.log(`Server listening on port: ${port}...`));
