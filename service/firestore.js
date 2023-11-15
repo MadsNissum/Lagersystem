0e35bcd75c660d2db48aaa32e15e7dbba7304d09
Chrsurma <chrsurma@yahoo.dk>
2023-11-15 10:55:32 +0100

    Firebase initialized

---
+++ service/firestore.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app"
+import {getFirestore,
+    collection,
+    getDocs,
+    getDoc, 
+    doc, 
+    deleteDoc, 
+    addDoc,
+    updateDoc
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBd9SN8XxBIHhI1HdabdJfqt6HcveoYGoU",
+    authDomain: "lagersystem-4f134.firebaseapp.com",
+    projectId: "lagersystem-4f134",
+    storageBucket: "lagersystem-4f134.appspot.com",
+    messagingSenderId: "573614154864",
+    appId: "1:573614154864:web:b3a2ee053065bc1c3e00b5"
+  }
+
+  const firebase_app = initializeApp(firebaseConfig)
+  const db = getFirestore(firebase_app)
+  const collections = collection(db,'lagersystem')
