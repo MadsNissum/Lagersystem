e8abce6bffb1b37da396b6e619dff6e67c8c6382
HalfBrokenKeyboard <113020182+HalfBrokenKeyboard@users.noreply.github.com>
2023-11-15 11:15:37 +0100

    Added GUI for Products

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 // Imports
 import express, { response } from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import pug from 'pug';
 
 // Consts
 const app = express();
-const port = 80;
+const port = 8000;
+
+// Get the directory name using the current module's URL
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Middleware
-app.use(express.static('public'));
+app.set('views', `${__dirname}/assets/views`);
+app.set('view engine', 'pug');
+app.use(express.static('assets'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
 
 // Routes
 app.get('/', (request, response) => {
     response.send('Hello World!');
+})
+
+app.get('/products', (request, response) => {
+    response.render('products');
 })
 
 
