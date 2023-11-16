63a15ef683105f736616e92e72dff4f184de89ab
MadsOverggard <nissum_10@hotmail.com>
2023-11-16 12:04:21 +0100

    Cleaned up firestore.js

--- service/firestore.js
+++ service/firestore.js
@@ -1,13 +1,5 @@
 import { initializeApp } from "firebase/app"
-import {getFirestore,
-    collection,
-    getDocs,
-    getDoc, 
-    doc, 
-    deleteDoc, 
-    addDoc,
-    updateDoc
-} from 'firebase/firestore'
+import {getFirestore, collection, getDocs, getDoc, doc, deleteDoc, addDoc, updateDoc} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBd9SN8XxBIHhI1HdabdJfqt6HcveoYGoU",
@@ -16,38 +8,45 @@ const firebaseConfig = {
     storageBucket: "lagersystem-4f134.appspot.com",
     messagingSenderId: "573614154864",
     appId: "1:573614154864:web:b3a2ee053065bc1c3e00b5"
-  };
+};
 
 const firebase_app = initializeApp(firebaseConfig)
-export const db = getFirestore(firebase_app)
+const db = getFirestore(firebase_app)
 const productCollection = collection(db, 'products')
 
-const getProducts = async () => {
-    let productsQueryDocs = await getDocs(productCollection);
-    let products = productsQueryDocs.docs.map(doc => {
-      let data = doc.data();
-      data.docID = doc.id;
-      return data;
-    });
-    return products;
-  }
-
-async function main() {
-    let data = await getProducts();
-    console.log(data);
+/**
+ * 
+ * @returns 
+ */
+async function getProducts() {
+  let productQueryDocs = await getDocs(productCollections)
+  let products = productQueryDocs.docs.map(doc => {
+      let data = doc.data()
+      data.docID = doc.id
+      return data
+  })
+  return products
 }
 
-main();
-
-const getProduct = async (id) => {
-    const docRef = doc(db, 'products', id)
-    const productQueryDoc = await getDoc(docRef)
-    let product = productQueryDoc.data()
-    product.docID = productQueryDoc.id
-    return product
+/**
+ * 
+ * @param {*} id 
+ * @returns 
+ */
+async function getProduct(id) {
+  const docRef = doc(db, 'products', id)
+  const productQueryDoc = await getDoc(docRef)
+  let product = productQueryDoc.data()
+  product.docID = productQueryDoc.id
+  return product
 }
 
+/**
+ * 
+ * @param {*} id 
+ */
+async function deleteProduct(id) {
+  const deletedProduct = await deleteDoc(db, 'products, id')
+}
 
-
-
-export default {getProduct, deleteProduct, firebase_app, firebaseConfig };
+export default { getProducts, getProduct, deleteProduct };
