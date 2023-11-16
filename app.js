655f9935972e0b4b9c59d3401ee1cd05b54aa2f7
HalfBrokenKeyboard <113020182+HalfBrokenKeyboard@users.noreply.github.com>
2023-11-16 10:18:28 +0100

    Products page ready for get request

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ import pug from 'pug';
 const app = express();
 const port = 8000;
 
+// Fake data delete when get request works!!!! - Lucas
+let productsArray = [
+    { brand: "Tuborg Classic", quantity: 10, price: 20, dueDate: new Date("2023-12-01"), location: "Skåde" },
+    { brand: "Brezzer", quantity: 5, price: 15, dueDate: new Date("2023-11-30"), location: "Aarhus" },
+    { brand: "Mokai", quantity: 8, price: 25, dueDate: new Date("2023-12-05"), location: "Vejle" },
+    { brand: "Heineken", quantity: 15, price: 18, dueDate: new Date("2023-12-10"), location: "Horsens" },
+    { brand: "Carlsberg", quantity: 12, price: 22, dueDate: new Date("2023-12-08"), location: "København" }
+  ];
+  
 // Get the directory name using the current module's URL
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -26,7 +35,7 @@ app.get('/', (request, response) => {
 })
 
 app.get('/products', (request, response) => {
-    response.render('products');
+    response.render('products', {products: productsArray});
 })
 
 
