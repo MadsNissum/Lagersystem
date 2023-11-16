c9b88536d75f99c3dab1e0856dd66e0feabeb74b
Ayyminq <amin.dahir@hotmail.com>
2023-11-16 10:50:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Imports
-import express from 'express';
+import express, { response } from 'express';
 import Product from './model/Product.js'
 import firestore from './service/firestore';
+import productsDBFunctions from './service/productsDBFunctions.js';
 
 // Consts
 const app = express();
@@ -13,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // Routes
-app.get('/', (request, response) => {
+app.get('/products', (request, response) => {
     let produkt = new Product('Carlsberg',50,Date.parse("2023-11-15"),"Skåde");
 
     response.send(produkt);
