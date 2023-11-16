8914e1a47ba6fdd953e7506f527e061bc43ac578
MadsOverggard <nissum_10@hotmail.com>
2023-11-16 12:23:51 +0100

    Update firestore.js

--- service/firestore.js
+++ service/firestore.js
@@ -1,62 +1,42 @@
 import { initializeApp } from "firebase/app"
-import {getFirestore,
-    collection,
-    getDocs,
-    getDoc, 
-    doc, 
-    deleteDoc, 
-    addDoc,
-    updateDoc
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  deleteDoc,
+  addDoc,
+  updateDoc
 } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBd9SN8XxBIHhI1HdabdJfqt6HcveoYGoU",
-    authDomain: "lagersystem-4f134.firebaseapp.com",
-    projectId: "lagersystem-4f134",
-    storageBucket: "lagersystem-4f134.appspot.com",
-    messagingSenderId: "573614154864",
-    appId: "1:573614154864:web:b3a2ee053065bc1c3e00b5"
-  };
+  apiKey: "AIzaSyBd9SN8XxBIHhI1HdabdJfqt6HcveoYGoU",
+  authDomain: "lagersystem-4f134.firebaseapp.com",
+  projectId: "lagersystem-4f134",
+  storageBucket: "lagersystem-4f134.appspot.com",
+  messagingSenderId: "573614154864",
+  appId: "1:573614154864:web:b3a2ee053065bc1c3e00b5"
+};
 
-  const firebase_app = initializeApp(firebaseConfig)
-  const db = getFirestore(firebase_app)
-  const productCollections = collection(db,'lagersystem')
+const firebase_app = initializeApp(firebaseConfig)
+const db = getFirestore(firebase_app)
+const productCollections = collection(db, 'products')
 
 
 const getProduct = async (id) => {
-    const docRef = doc(db, 'products', id)
-    const productQueryDoc = await getDoc(docRef)
-    let product = productQueryDoc.data()
-    product.docID = productQueryDoc.id
-    return product
+  const docRef = doc(db, 'products', id)
+  const productQueryDoc = await getDoc(docRef)
+  let product = productQueryDoc.data()
+  product.docID = productQueryDoc.id
+  return product
 }
 
 
 
-function registerProducts(Brand, price, expiration, location, amount) {
-    for(let i = 0; i < amount; i++) {
-        let product = new Product(Brand, price, expiration, location);
-        firestore.addProduct(product)
-    }
+async function addProduct(product) {
+  await addDoc(productCollections, product)
 }
 
-registerProducts("vand", 50, Date.parse("2023-11-16"), "skåde", 20);
 
-
-const firestoreAdd = {
-    addProduct: async function(product) {
-        try {
-            const docRef = await firestore.collection('products').addProduct({
-                Brand: product.Brand,
-                price: product.price,
-                expiration: product.expiration
-            });
-            console.log('Product added with ID ', docRef.id)
-            return docRef.id;
-        } catch (error) {
-            console.error('Error adding product: ', error)
-        }
-    }
-}
-
-  export default {getProduct, deleteProduct};
+export default { getProduct, deleteProduct };
