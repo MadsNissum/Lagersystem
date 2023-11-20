249aed9383f90133b3a1f82a43c371f7c580afe5
MadsOverggard <nissum_10@hotmail.com>
2023-11-20 11:49:49 +0100

    Update observer.js

--- service/observer.js
+++ service/observer.js
@@ -8,45 +8,49 @@ const transporter = nodemailer.createTransport({
     host: "smtp.office365.com",
     auth: {
         user: 'LagerSystemSkaade@hotmail.com',
-        pass: 'Gruppenersej123',
+        pass: 'Gruppenersej123'
     },
     secure: false,
 });
-
-
 
 
 notifyPeople()
 
 export async function notifyPeople() {
     const product = await firestore.getProducts();
+    let array = [];
 
     product.forEach(product => {
         let date = new Date();
         date.setDate(date.getDate() + 10);
 
         if (product.getDate() == date.toISOString().split('T')[0]) {
-
             const brandText = String(product.brand);
-            const exDate = String(product.getDate())
-
-            const mailData = {
-                from: 'LagerSystemSkaade@hotmail.com',  // sender address
-                replyTo: 'LagerSystemSkaade@hotmail.com',
-                to: 'mikkelhess@icloud.com, nissum_10@hotmail.com',   // list of receivers
-                subject: `${brandText} er ved at udløbe på dato!`,
-                text: `${brandText} udløber her den ${exDate}`,
-                html: `${brandText} udløber her den <b>${exDate}</b>`,
-            };
-
-            transporter.sendMail(mailData, (err, info) => {
-                if (err)
-                    console.log(err)
-                else
-                    console.log(info);
-            });
-
-
+            const exDate = String(product.getDate());
+            array.push({text: brandText, date: exDate});
         }
     })
+
+    if (array.length != 0) {
+        let html = "";
+
+        array.forEach(product => {
+            html += `${product.text} udløber her den <b>${product.date}</b><br>`;
+        })
+
+        const mailData = {
+            from: 'LagerSystemSkaade@hotmail.com',  // sender address
+            replyTo: 'LagerSystemSkaade@hotmail.com',
+            to: 'mikkelhess@icloud.com, nissum_10@hotmail.com',   // list of receivers
+            subject: `Inventar er ved at udløbe på dato!`,
+            html: html,
+        };
+    
+        transporter.sendMail(mailData, (err, info) => {
+            if (err)
+                console.log(err)
+            else
+                console.log(info);
+        });
+    }
 }
