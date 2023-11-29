663cc5f33ac5a0d589dbd9436e538dda3aaac5b6
Mikkelhess <mikkelhess@icloud.com>
2023-11-29 11:42:03 +0100

    Add & Get Email

--- test/testEmailDB.js
+++ test/testEmailDB.js
@@ -4,21 +4,48 @@ import { getEmail, addEmail, getEmails, deleteEmail, updateEmail } from '../data
 
 describe('EmailDB Test',()=>{
 
-    it('It should add the email to the database', async ()=>{
 
-        let email = 'test@test.com'
+    describe('Add Email function',()=>{
 
-        let emailDoc = await addEmail({email: email})
-
-        let addedEmail = await getEmail(emailDoc.id)
-
-        assert.strictEqual(email,addedEmail.email)
-
-        deleteEmail(emaildoc.id)
+        it('It should add the email to the database', async ()=>{
+    
+            let email = 'LagerSystemSkaade@hotmail.com'
+    
+            let emailDoc = await addEmail({email: email})
+    
+            let addedEmail = await getEmail(emailDoc.id)
+    
+            assert.strictEqual(addedEmail.email,email)
+    
+            deleteEmail(emailDoc.id)
+        })
+    
     })
-
-    it('Should return the correct email',()=>{
-        
+    
+    describe('Get Email function',()=>{
+    
+        it('Should return the correct email', async ()=>{
+    
+            let email = 'LagerSystemSkaade@hotmail.com';
+    
+            let emailDoc = await addEmail({email: email})
+    
+            let gettedEmail = await getEmail(emailDoc.id)
+    
+            assert.strictEqual(gettedEmail.email,email)
+    
+            deleteEmail(emailDoc.id)
+    
+        })
+    
+        it('Should return null if the email doesnt exist', async ()=>{
+    
+            let email = await getEmail('test')
+    
+            assert.strictEqual(email,null)
+    
+        })
+    
     })
-
+    
 })
