271c050a51a239c6e41e71a511da0a3ccd3f4661
Mikkelhess <mikkelhess@icloud.com>
2023-11-30 11:00:12 +0100

    Update testLogin.js

--- test/service/testLogin.js
+++ test/service/testLogin.js
@@ -1,6 +1,28 @@
 // i am tha test monkeyyyyyyyyyyyyyyyyyyyyy :-)
 
+import { getAccount } from "../../database/loginDB.js"
+import { createAccount } from "../../service/login.js"
+import assert from 'assert'
 
 describe('Login Test',()=>{
+
+    it('Create account function', async ()=>{
+
+        await createAccount('testAccount','kaninpis')
+
+        let account = getAccount('testAccount')
+        
+        let accountExists = false;
+
+        if(account.username === 'testAccount') {
+            accountExists = true;
+        }
+
+        assert.strictEqual(accountExists,true)
+
+    })
+
+  
+
 
 })
